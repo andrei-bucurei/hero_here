2f75b5b31c96f9361b6f0ea11d2cbb8cb76de879
Igor Kurilenko <igorkurilenko@icloud.com>
2024-06-20 10:15:19 +0300

    Activate hero transitions when a message is sent

--- example/lib/send_message_hero/main.dart
+++ example/lib/send_message_hero/main.dart
@@ -14,6 +14,7 @@
 
 import 'package:example/send_message_hero/message_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:hero_here/hero_here.dart';
 
 import 'message.dart';
 
@@ -40,27 +41,35 @@ class _HeroHereExampleState extends State<HeroHereExample> {
   final _focusNode = FocusNode();
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(forceMaterialTransparency: true),
-        body: ListView.builder(
-          controller: _scrollController,
-          itemCount: _messages.length,
-          itemBuilder: (context, index) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32),
-            child: _buildMessage(_messages[index]),
+  Widget build(BuildContext context) => HeroHereSwitcher(
+        child: Scaffold(
+          extendBody: true,
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(forceMaterialTransparency: true),
+          body: ListView.builder(
+            controller: _scrollController,
+            itemCount: _messages.length,
+            itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: _buildMessage(_messages[index]),
+            ),
           ),
-        ),
-        bottomNavigationBar: Padding(
-          padding: const EdgeInsets.all(32),
-          child: _buildMessageInput(),
+          bottomNavigationBar: Padding(
+            padding: const EdgeInsets.all(32),
+            child: _buildMessageInput(),
+          ),
         ),
       );
 
-  Widget _buildMessage(Message message) => MessageWidget.readOnly(
-        text: message.textOrEmpty,
-        decoration: const InputDecoration(
-          border: OutlineInputBorder(
-            borderSide: BorderSide.none,
+  Widget _buildMessage(Message message) => HeroHere(
+        tag: '$message',
+        key: ValueKey('published-$message}'),
+        child: MessageWidget.readOnly(
+          text: message.textOrEmpty,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
           ),
         ),
       );
@@ -68,15 +77,19 @@ class _HeroHereExampleState extends State<HeroHereExample> {
   Widget _buildMessageInput() {
     final message = Message();
 
-    return MessageWidget.input(
-      focusNode: _focusNode,
-      onSubmitted: (text) => _onMessageSubmitted(message..text = text),
-      decoration: const InputDecoration(
-        hintText: 'Message',
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.all(
-            Radius.circular(32),
+    return HeroHere(
+      tag: '$message',
+      key: ValueKey('$message'),
+      child: MessageWidget.input(
+        focusNode: _focusNode,
+        onSubmitted: (text) => _onMessageSubmitted(message..text = text),
+        decoration: const InputDecoration(
+          hintText: 'Message',
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.all(
+              Radius.circular(32),
+            ),
           ),
         ),
       ),
@@ -100,8 +113,8 @@ class _HeroHereExampleState extends State<HeroHereExample> {
 
   void _scrollToBottom() => _scrollController.animateTo(
         _scrollController.position.maxScrollExtent,
-        duration: const Duration(milliseconds: 500),
-        curve: Curves.linear,
+        duration: HeroHere.defaultFlightAnimationDuration,
+        curve: HeroHere.defaultFlightAnimationCurve,
       );
 }
 
